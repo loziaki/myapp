7a9cb37574945ab56ae51bc3b32f91baa6bba440
loziaki <loziaki@126.com>
2018-03-09 15:20:52 +0800

    reuse the db connection in a request

--- include/Service/Util.php
+++ include/Service/Util.php
@@ -5,15 +5,19 @@ use \PDO;
 
 class Util
 {
+    public static $db;
     public static function getMySQLInstrance()
     {
         //初始化数据库连接
-        $dsn = 'mysql:host='.DB_LOCALOHOTS.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
         try {
-            $db = new PDO($dsn, DB_USER, DB_PASSWORD);
-            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $db;
+            if (!isset(self::$db)) {
+                $dsn = 'mysql:host='.DB_LOCALOHOTS.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
+                $db = new PDO($dsn, DB_USER, DB_PASSWORD);
+                $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$db = $db;
+            }
+            return self::$db;
         } catch (PDOException $e) {
             throw new Exception('QwQ MySQL CONNECTING ERROR: '. $e->getMessage());
         }
