694d4d3d12f407b2cafe0963c7d941869938e15f
677_on_ub2 <loziaki@126.com>
2018-09-03 09:51:53 +0800

    fix a bug in MyGuide

--- Framework/Decorator/MyGuide.php
+++ Framework/Decorator/MyGuide.php
@@ -15,39 +15,40 @@ class MyGuider extends Decoractor
     public function handle(): bool
     {
         if (!file_exists(self::ROUTES_PATH)) {
-            throw new \Framework\MyException('there is no routes.php',10101);
+            throw new Exception('there is no routes.php',10101);
         }
 
         if (empty($this->method) || empty($this->path)) {
-            throw new \Framework\MyException('bad path parameters',10102);
+            throw new Exception('bad path parameters',10102);
         }
 
         $routesMap = require self::ROUTES_PATH;
         if (!isset($routesMap[$this->method][$this->path])) {
-            throw new \Framework\MyException('no matching router',10105);
+            throw new Exception('no matching router',10105);
         }
 
         $route = $routesMap[$this->method][$this->path];
 
-        if (is_stinrg($route) && strpos($route,'@') > 0) {
+        if (is_stinrg($route)) {
             $middlewares = [];
-
+            $action = $route;
         } elseif (is_array($route)) {
             if (isset($route['middleware']) && is_string($route['middleware'])) {
                 $middlewares = explode(',',$route['middleware']);
             } else {
                 $middlewares = [];
             }
-
-            if (isset($route['action']) && is_string($route['action']) && strpos($route['action'],'@')) {
-                $arr = explode('@',$action);
-                $cname = 'Controller\\'.$arr[0];
-                $aname = $arr[1];
-            } else {
-                throw new \Framework\MyException('bad route config',10103);
-            }
+            $action = $route['action'];
         } else {
-            throw new \Framework\MyException('bad path parameters',10104);
+            throw new Exception('bad path parameters',10104);
+        }
+
+        if (is_string($route['action']) && strpos($route['action'],'@') > 0) {
+            $arr = explode('@',$action);
+            $cname = 'Controller\\'.$arr[0];
+            $aname = $arr[1];
+        } else {
+            throw new Exception('bad route config',10103);
         }
 
         $this->app->setCAM($cname,$aname,$middlewares);
