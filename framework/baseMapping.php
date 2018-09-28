ccf10e382dccdd50df43a40d00db16237bfcbb91
677_on_ub2 <loziaki@126.com>
2018-09-28 20:06:39 +0800

    move all the Drivers to another new floder

--- framework/baseMapping.php
+++ framework/baseMapping.php
@@ -6,8 +6,8 @@ return array(
     'Framework\MiddlewareInterface' => ROOT_PATH.'framework/MiddlewareInterface.php',
     'Framework\Handle' => ROOT_PATH.'framework/Handle.php',
     'Framework\Decorator\Decorator' => ROOT_PATH.'framework/Decorator/Decorator.php',
-    'Framework\Decorator\Driver' => ROOT_PATH.'framework/Decorator/Driver.php',
-    'Framework\Decorator\MyCgiDriver' => ROOT_PATH.'framework/Decorator/MyCgiDriver.php',
-    'Framework\Decorator\MyCliDriver' => ROOT_PATH.'framework/Decorator/MyCliDriver.php',
+    'Framework\Decorator\Driver' => ROOT_PATH.'framework/Decorator/Driver/Driver.php',
+    'Framework\Decorator\MyCgiDriver' => ROOT_PATH.'framework/Decorator/Driver/MyCgiDriver.php',
+    'Framework\Decorator\MyCliDriver' => ROOT_PATH.'framework/Decorator/Driver/MyCliDriver.php',
     'Framework\Decorator\MyGuide' => ROOT_PATH.'framework/Decorator/MyGuide.php',
 );
