8f6723715670701b603c4d57abf65178fda027cb
loziaki <loziaki@126.com>
2019-01-22 16:14:11 +0800

    move the Routing module to antoher place

--- framework/Routing/compile.php
+++ framework/Routing/compile.php
@@ -4,7 +4,7 @@ define('BASE_DIR', dirname(__FILE__).'/');
 require BASE_DIR.'RouteCompiler.php';
 
 $routes_file_path = BASE_DIR.'routes.php';
-$output_path = BASE_DIR.'../../routes.php';
+$output_path = BASE_DIR.'../../include/routes.php';
 
 MyApp\Routing\RouteCompiler::compile($routes_file_path)->output($output_path);
 
